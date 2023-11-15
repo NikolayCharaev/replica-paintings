6c3ebeb506a0b992586e679c7fd5b9997effa16f
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-11-15 21:17:56 +0300

    project init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+   <>
+
+
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas omnis velit adipisci, veniam, nisi quam repudiandae, hic consequuntur id accusamus expedita culpa beatae officia ut facere in repellat consectetur est?
+   </>
+  )
+}
---
+++ sanity/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'replica-paintings',
+
+  projectId: '5l4p48au',
+  dataset: 'replica-paintings',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
